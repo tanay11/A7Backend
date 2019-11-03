44b9a580fdd4a436a7188511d5a30d4c68e0d96b
Tanay Mainkar <tanay.mainkar@lntinfotech.com>
2019-11-04 00:48:16 +0530

    trying sendgrid

--- index.js
+++ index.js
@@ -67,8 +67,8 @@ app.post("/api/form", (req, res) => {
 		}
 	}));
 	const mailOptions = {
-		from: "tanaymainkar25@gmail.com", // sender address
-		to: "tanaymainker25@gmail.com",
+		from: "a7.nasik@gmail.com", // sender address
+		to: "tanaymainkar25@gmail.com",
 		cc: "",
 		subject: "Registration successful", // Subject line
 		html: `<div><h2>Lucian Paints Welcomes You..</h2> <br/>Details -  ${JSON.stringify(req.body)}</div>`
