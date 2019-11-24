4346f23bac453f0cb6d3ddcd72b04f3f11cf6a37
Tanay Mainkar <tanay.mainkar@lntinfotech.com>
2019-11-24 22:49:15 +0530

    Try my api key

--- index.js
+++ index.js
@@ -4,13 +4,28 @@ const bodyParser = require('body-parser')
 const nodemailer = require('nodemailer');
 const sgTransport = require('nodemailer-sendgrid-transport');
 const smtpTransport = require('nodemailer-smtp-transport');
-const sendgrid = require('sendgrid')("tanaymainkar25", "tannu#209141")
 const fs = require('fs')
 const http = require('http')
 const path = require('path')
 const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
+
+app.post("/api/form", (req, res) => {
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+  to: 'tanaymainkar25@gmail.com',
+  from: 'a7.nasik@gmail.com',
+  subject: 'Registration successful',
+  text: 'Lucian Paints - Kam Daam , Damdaar Kaam',
+  html: `<div><h2>Lucian Paints Welcomes You..</h2> <br/>Details -  ${JSON.stringify(req.body)}</div>`,
+};
+sgMail.send(msg);
+console.log("Isse pehle chal")
+res.send("trying things workin out")
+})
+
 const cors = (req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
 	res.header(
@@ -33,64 +48,46 @@ app.get("/", (req, res) => {
 });
 
 
+
 // app.post("/api/form", (req, res) => {
 // 	//res.send(bodyParser(req));
 // 	//console.log(bodyParser(req));
 // 	console.log("Alag sa identify", req.body.email)
-// 	sendgrid.send({
-// 		to:"tanaymainker25@gmail.com",
-// 		from:"tanaymainkar25@gmail.com",
-// 		subject:"Hello World",
-// 		text:"Try karte hai"
-// 	},function(err,res){
-// 		if(err){
-// 			console.log("ab kaise hoga",err)
+// 	res.setHeader("Content-Type", "application/json");
+// 	res.write("you posted:\n");
+// 	res.end(JSON.stringify(req.body, null, 2));
+// 	var transporter = nodemailer.createTransport(sgTransport({
+// 		pool: true,
+// 		host: "smtp.sendgrid.net",
+// 		port: 587,
+// 		secure: false,
+// 		auth: {
+// 			Username: process.env.user,
+// 			Password: process.env.key
 // 		}
-// 		else{
-// 			console.log("chal k dikha de",res)
+// 	}));
+// 	const mailOptions = {
+// 		from: "a7.nasik@gmail.com", // sender address
+// 		to: "tanaymainkar25@gmail.com",
+// 		cc: "tanaymainker25@gmail.com",
+// 		subject: "Registration successful", // Subject line
+// 		html: `<div><h2>Lucian Paints Welcomes You..</h2> <br/>Details -  ${JSON.stringify(req.body)}</div>`
+
+// 		// plain text body
+// 	};
+// 	transporter.sendMail(mailOptions, function (err, info) {
+// 		if (err) console.log(err);
+// 		else console.log(info);
+// 	});
+// 	// verify connection configuration
+// 	transporter.verify(function (error, success) {
+// 		if (error) {
+// 			console.log(error);
+// 		} else {
+// 			console.log("Server is ready to take our messages", success);
 // 		}
-// 	})
+// 	});
 // });
-
-app.post("/api/form", (req, res) => {
-	//res.send(bodyParser(req));
-	//console.log(bodyParser(req));
-	console.log("Alag sa identify", req.body.email)
-	res.setHeader("Content-Type", "application/json");
-	res.write("you posted:\n");
-	res.end(JSON.stringify(req.body, null, 2));
-	var transporter = nodemailer.createTransport(sgTransport({
-		pool: true,
-		host: "smtp.sendgrid.net",
-		port: 587,
-		secure: false,
-		auth: {
-			api_user: process.env.api_user,
-			api_key: process.env.api_key
-		}
-	}));
-	const mailOptions = {
-		from: "a7.nasik@gmail.com", // sender address
-		to: req.body.email,
-		cc: "tanaymainker25@gmail.com",
-		subject: "Registration successful", // Subject line
-		html: `<div><h2>Lucian Paints Welcomes You..</h2> <br/>Details -  ${JSON.stringify(req.body)}</div>`
-
-		// plain text body
-	};
-	transporter.sendMail(mailOptions, function (err, info) {
-		if (err) console.log(err);
-		else console.log(info);
-	});
-	// verify connection configuration
-	transporter.verify(function (error, success) {
-		if (error) {
-			console.log(error);
-		} else {
-			console.log("Server is ready to take our messages", success);
-		}
-	});
-});
 
 
 // var helper = require('sendgrid').mail;
